7267bbd75855ef00e644486242ea1b2a7ef92151
shuko hirano <shuko.hirano@ibm.com>
2023-06-04 20:46:56 +0900

    Added List Items

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,68 +1,141 @@
 <script setup>
-import { ref } from "vue";
+import { ref, reactive, onMounted, computed } from 'vue'
+import axios from 'axios'
 import { ArrowRight, Money } from '@element-plus/icons-vue'
+import sampleData from './test.json'
 
-let isBadge = ref(true);
-let allowNext = ref(true);
-const cardOnclick = () => {
-  isBadge.value = !(isBadge.value);
-  allowNext.value = false;
+let catalogItems = ref([])
+let state = reactive({
+  selectedItems: [],
+  error: null,
+  loading: false
+})
+
+let cardOnclick = (itemId) => {
+  if (state.selectedItems.includes(itemId)) {
+    state.selectedItems = state.selectedItems.filter((id) => id !== itemId)
+  } else {
+    state.selectedItems.push(itemId)
+  }
 }
+let isItemSelected = (id) => computed(() => state.selectedItems.includes(id))
+let hasSelectedItems = computed(() => state.selectedItems && state.selectedItems.length > 0)
+
+const fetchCatalogItems = async () => {
+  state.loading = true
+  try {
+    const response = await axios.get('/api/catalog/items', {
+      params: {
+        category: 'CoffeeBean'
+      }
+    })
+
+    if (response.status === 200) {
+      catalogItems.value = response.data
+    }
+  } catch (error) {
+    console.error(error)
+    state.error = error.message
+  } finally {
+    state.loading = false
+  }
+}
+
+onMounted(fetchCatalogItems)
 </script>
 
 <template>
-  <el-main>
-    <el-breadcrumb :separator-icon="ArrowRight" style="margin-bottom:15px;">
-      <el-breadcrumb-item class="active-bread">Select Coffee Beans</el-breadcrumb-item>
-      <el-breadcrumb-item class="non-active-bread">Determine the percentage of blending</el-breadcrumb-item>
-      <el-breadcrumb-item class="non-active-bread">Order</el-breadcrumb-item>
-    </el-breadcrumb>
-    <el-row :gutter="20" style="margin-bottom:15px;">
-      <el-col :span="6">
-        <el-badge value="Select" type="primary" :hidden="isBadge">
-          <el-card :body-style="{ padding: '0px' }" @click="cardOnclick()">
-            <el-image style="width: 100%; height:200px;" src="" fit="cover" />
-            <div style="margin:10px">
-              <div class="coffee-title">
-                <el-text tag="b" size="large">Hawaii Kona</el-text>
-                <el-text><el-icon><Money /></el-icon> ¥20/g</el-text>
-              </div>
-              <div class="coffee-desc">
-                <el-text size="small">
-                  Kona coffee is characterised by soft acidity and smooth taste. It is characterised by low bitterness, soft acidity and a fruity-sweet aroma that evokes the original fruitiness of coffee. Rather than being full-bodied, it offers a refreshing aftertaste.
-                </el-text>
-              </div>
-            </div>
-          </el-card>
-        </el-badge>
-      </el-col>
-    </el-row>
-  </el-main>
-  <el-footer>
-    <el-button class="next-btn" :icon="ArrowRight" type="primary" size="large" :disabled="allowNext">Next Step</el-button>
-  </el-footer>
+  <div>
+    <el-main>
+      <el-breadcrumb :separator-icon="ArrowRight" style="margin-bottom: 15px">
+        <el-breadcrumb-item class="active-bread">Select Coffee Beans</el-breadcrumb-item>
+        <el-breadcrumb-item class="non-active-bread"
+          >Determine the percentage of blending</el-breadcrumb-item
+        >
+        <el-breadcrumb-item class="non-active-bread">Order</el-breadcrumb-item>
+      </el-breadcrumb>
+      <el-main v-loading.fullscreen.lock="state.loading">
+        <el-row :gutter="20" style="margin-bottom: 15px">
+          <el-col
+            :span="6"
+            v-for="(item, index) in catalogItems"
+            :key="index"
+            style="margin-bottom: 15px"
+          >
+            <el-badge
+              value="Select"
+              type="primary"
+              :hidden="!isItemSelected(item.catalogItemObj.catalogItemObj.id).value"
+            >
+              <el-card
+                :body-style="{ padding: '0px' }"
+                @click="cardOnclick(item.catalogItemObj.catalogItemObj.id)"
+                style="user-select: none"
+              >
+                <el-image
+                  style="width: 100%; height: 200px"
+                  :src="item.catalogItemObj.catalogImageObj.imageData.url"
+                  fit="cover"
+                />
+                <div style="margin: 10px">
+                  <div class="coffee-title">
+                    <el-text tag="b" size="large">{{
+                      item.catalogItemObj.catalogItemObj.itemData.name
+                    }}</el-text>
+                    <el-text
+                      ><el-icon><Money /></el-icon> 💲{{
+                        item.catalogItemObj.catalogItemObj.itemData.variations[0].itemVariationData
+                          .priceMoney.amount / 100
+                      }}/g</el-text
+                    >
+                  </div>
+                  <div class="coffee-desc">
+                    <el-text size="small">{{
+                      item.catalogItemObj.catalogItemObj.itemData.description
+                    }}</el-text>
+                  </div>
+                </div>
+              </el-card>
+            </el-badge>
+          </el-col>
+        </el-row>
+      </el-main>
+    </el-main>
+    <el-footer>
+      <el-button
+        class="next-btn"
+        :icon="ArrowRight"
+        type="primary"
+        size="large"
+        :disabled="hasSelectedItems"
+        >Next Step</el-button
+      >
+    </el-footer>
+    <div v-if="state.error">
+      <p>Error: {{ state.error }}</p>
+    </div>
+  </div>
 </template>
 
-
 <style scoped>
-.coffee-title{
+.coffee-title {
   display: flex;
   justify-content: space-between;
 }
-.coffee-desc{
+.coffee-desc {
   line-height: 17px;
 }
-.el-card:active{
-  background-color: #E6E8EB;
-  border: solid #409EFF thin;
+.el-card:active {
+  background-color: #e6e8eb;
+  border: solid #409eff thin;
 }
-.el-badge{
+.el-badge {
   height: 100%;
 }
-.el-card{
+.el-card {
   height: 100%;
 }
-.next-btn{
+.next-btn {
   width: 80vw;
   display: block;
   margin: 0 auto;
